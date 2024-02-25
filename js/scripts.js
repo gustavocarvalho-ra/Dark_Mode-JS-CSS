2c457d2d569d89922d579204e1e5be053f4861b6
GustavoCR021 <gustavoc1922@gmail.com>
2024-02-25 16:28:49 -0300

    at5/f

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,28 @@
 const changeThemeBtn = document.querySelector("#change-theme")
 
-changeThemeBtn.addEventListener("change", function() {
+// Toggle dark mode
+function toggleDarkMode () {
   document.body.classList.toggle("dark")
-})
+}
+
+// Load light or dark mode
+function loadTheme() {
+  const darkMode = localStorage.getItem("dark")
+
+  if(darkMode) {
+    toggleDarkMode();
+  }
+}
+
+
+changeThemeBtn.addEventListener("change", function() {
+  toggleDarkMode();
+
+  // Save or remove dark mode
+  localStorage.removeItem("dark");
+
+  if(document.body.classList.contains("dark")) {
+    localStorage.setItem("dark", 1);
+  }
+
+});
